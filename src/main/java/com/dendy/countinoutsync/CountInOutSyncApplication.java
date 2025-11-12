9d49856d9009529d494ea83e987e7f4b0a0b139c
dendy192 <dtiawan192@gmail.com>
2025-11-12 22:23:35 +0700

    first commit

---
+++ src/main/java/com/dendy/countinoutsync/CountInOutSyncApplication.java
@@ -0,0 +1,13 @@
+package com.dendy.countinoutsync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountInOutSyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CountInOutSyncApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dendy/countinoutsync/CountInOutSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dendy.countinoutsync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountInOutSyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
